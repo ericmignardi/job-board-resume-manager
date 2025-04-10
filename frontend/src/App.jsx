e1783554883c254091b37e26feeb03affaf47dbb
Eric Mignardi <mignardi.e@gmail.com>
2025-04-10 11:30:59 -0400

    Deleted frontend components & pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,84 +1,13 @@
-import React, { useEffect } from "react";
-import { Route, Routes, Navigate } from "react-router-dom";
+import React from "react";
+import Toaster from "react-hot-toast";
+import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage.jsx";
-import DashboardLayout from "./pages/DashboardLayout.jsx";
-import JobListingsPage from "./pages/JobListingsPage.jsx";
-import JobDetailsPage from "./pages/JobDetailsPage.jsx";
-import EmployerJobListingsPage from "./pages/EmployerJobListingsPage.jsx";
-import EmployerApplicationsPage from "./pages/EmployerApplicationsPage.jsx";
-import ApplicationsPage from "./pages/ApplicationsPage.jsx";
-import NotFoundPage from "./pages/NotFoundPage.jsx";
-import { useAuthStore } from "./stores/useAuthStore.js";
-import EmployerApplicationDetailsPage from "./pages/EmployerApplicationDetailsPage.jsx";
-import EmployerJobDetailsPage from "./pages/EmployerJobDetailsPage.jsx";
-import { Toaster } from "react-hot-toast";
 
 const App = () => {
-  const { authUser, isVerifyingAuth, verify } = useAuthStore();
-
-  useEffect(() => {
-    verify();
-  }, [verify]);
-
-  if (isVerifyingAuth && !authUser) return <div className="loading"></div>;
-
   return (
     <>
       <Routes>
-        {/* HOME */}
         <Route path="/" element={<HomePage />} />
-
-        {/* DASHBOARD */}
-        <Route path="/dashboard" element={<DashboardLayout />}>
-          <Route index element={<JobListingsPage />} />{" "}
-          <Route path="jobs" element={<JobListingsPage />} />
-          <Route path="jobs/:jobId" element={<JobDetailsPage />} />
-          <Route path="applications" element={<ApplicationsPage />} />
-          {/* EMPLOYER DASHBOARD */}
-          <Route
-            path="employer/jobs"
-            element={
-              authUser && authUser.role === "employer" ? (
-                <EmployerJobListingsPage />
-              ) : (
-                <Navigate to="/" replace />
-              )
-            }
-          />
-          <Route
-            path="employer/jobs/:jobId"
-            element={
-              authUser && authUser.role === "employer" ? (
-                <EmployerJobDetailsPage />
-              ) : (
-                <Navigate to="/" replace />
-              )
-            }
-          />
-          <Route
-            path="employer/applications"
-            element={
-              authUser && authUser.role === "employer" ? (
-                <EmployerApplicationsPage />
-              ) : (
-                <Navigate to="/" replace />
-              )
-            }
-          />
-          <Route
-            path="employer/applications/:applicationId"
-            element={
-              authUser && authUser.role === "employer" ? (
-                <EmployerApplicationDetailsPage />
-              ) : (
-                <Navigate to="/" replace />
-              )
-            }
-          />
-        </Route>
-
-        {/* OTHER */}
-        <Route path="*" element={<NotFoundPage />} />
       </Routes>
       <Toaster />
     </>
